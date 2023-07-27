35c064ccb76991635cab9d1c2aa81aa6821112d2
Lito Banano <135684502+hyperlitz@users.noreply.github.com>
2023-07-26 18:26:32 -0700

    First commit
    Unpolish file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# fitnessplus
+# Fitness Tracker API
+
+Welcome to the Fitness Tracker API! This API allows you to track your fitness activities, set goals, and monitor your progress. Whether you are a fitness enthusiast, athlete, or just someone trying to stay active, this API is designed to help you achieve your fitness goals and maintain a healthy lifestyle.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+- [API Endpoints](#api-endpoints)
+  - [User](#user)
+  - [Activities](#activities)
+  - [Goals](#goals)
+- [Authentication](#authentication)
+- [Error Handling](#error-handling)
+- [Examples](#examples)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The Fitness Tracker API allows users to create an account, log various fitness activities (e.g., running, cycling, swimming), set personal fitness goals, and retrieve progress reports. The API is built using RESTful principles and returns data in JSON format.
+
+## Getting Started
+
+### Prerequisites
---
+++ assets/js/mainscript.js
@@ -0,0 +1,134 @@
+const loader = document.getElementById('loader');
+    // Fitness Workout Planner
+    document.getElementById('workoutForm').addEventListener('submit', async function (event) {
+      event.preventDefault();
+      
+      loader.style.display = 'block';
+      const time = document.getElementById('time').value;
+      const muscle = document.getElementById('muscle').value;
+      const location = document.getElementById('location').value;
+      const equipment = document.getElementById('equipment').value;
+
+      const url = `https://workout-planner1.p.rapidapi.com/?time=${time}&muscle=${muscle}&location=${location}&equipment=${equipment}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'fff2f9c442msha7d88f7ff4354cbp17c8b7jsn598c2d7957dc',
+          'X-RapidAPI-Host': 'workout-planner1.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        loader.style.display = 'none';
+
+        const warmUpTable = document.getElementById('warmUpTable');
+        warmUpTable.innerHTML = `
+          <tr>
+            <th>Exercise</th>
+            <th>Time</th>
+          </tr>
+          ${data['Warm Up'].map(item => `
+            <tr>
+              <td>${item.Exercise}</td>
+              <td>${item.Time}</td>
+            </tr>
+          `).join('')}
+        `;
+
+        const exercisesTable = document.getElementById('exercisesTable');
+        exercisesTable.innerHTML = `
+          <tr>
+            <th>Exercise</th>
+            <th>Sets</th>
+            <th>Reps</th>
+          </tr>
+          ${data.Exercises.map(item => `
+            <tr>
+              <td>${item.Exercise}</td>
+              <td>${item.Sets}</td>
+              <td>${item.Reps}</td>
+            </tr>
+          `).join('')}
+        `;
+
+        const coolDownTable = document.getElementById('coolDownTable');
+        coolDownTable.innerHTML = `
+          <tr>
+            <th>Exercise</th>
+            <th>Time</th>
+          </tr>
+          ${data['Cool Down'].map(item => `
+            <tr>
+              <td>${item.Exercise}</td>
+              <td>${item.Time}</td>
+            </tr>
+          `).join('')}
+        `;
+
+        const workoutResultSection = document.getElementById('workoutResult');
+        workoutResultSection.style.display = 'block';
+      } catch (error) {
+        console.error(error);
+        loader.style.display = 'none';
+      }
+    });
+
+    // BMI Calculator
+    document.getElementById('calculate').addEventListener('click', calculateBMI);
+
+    async function calculateBMI() {
+      const age = parseInt(document.getElementById('age').value);
+      const gender = document.getElementById('gender').value;
+      const weight = parseFloat(document.getElementById('weight').value);
+      const height = parseFloat(document.getElementById('height').value);
+
+      if (isNaN(age) || age < 1) {
+        alert('Please enter a valid age.');
+        return;
+      }
+
+      if (gender === '') {
+        alert('Please select a valid gender.');
+        return;
+      }
+
+      if (isNaN(weight) || isNaN(height) || weight < 1 || height < 0.01) {
+        alert('Please enter valid weight and height values.');
+        return;
+      }
+
+      const url = `https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=${weight}&height=${height}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'fff2f9c442msha7d88f7ff4354cbp17c8b7jsn598c2d7957dc',
+          'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        displayBMIResult(result);
+      } catch (error) {
+        console.error(error);
+        alert('An error occurred while fetching data. Please try again later.');
+        loader.style.display = 'none';
+      }
+    }
+
+    function displayBMIResult(result) {
+      const bmiValue = result.bmi.toFixed(2);
+      const category = result.health;
+
+      const bmiResultSection = document.getElementById('bmiResult');
+      const bmiValueElement = document.getElementById('bmiValue');
+      const bmiCategoryElement = document.getElementById('bmiCategory');
+
+      bmiValueElement.textContent = bmiValue;
+      bmiValueElement.style.textAlign = "center";
+
+      bmiResultSection.style.display = 'block';
+    }
